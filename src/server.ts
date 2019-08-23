d96a47e8714aca962c4cf8c87184d205a2b146ab
ilinzweilin <ilinzweilin@ilinzweilins-MacBook-Pro.local>
2019-08-23 16:58:38 +0200

    use https instead of ssh for tinlake.js

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ import https from 'https';
 import http from 'http';
 import config from './config';
 
-console.log(config)
 const configurations = {
   // Note: You may need sudo to run on port 443
   production: { ssl: true, port: 443 },
