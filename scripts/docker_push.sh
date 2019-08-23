7b252a7e97a491c7ef636ed9df55bdfdfdc9b1bd
ilinzweilin <ilinzweilin@ilinzweilins-MacBook-Pro.local>
2019-08-23 15:27:14 +0200

    fix image name

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -4,7 +4,7 @@ BRANCH_ABBR=`git rev-parse --abbrev-ref HEAD`
 TRAVIS_BRANCH=${TRAVIS_BRANCH:-$BRANCH_ABBR}
 TIMESTAMP=`date -u +%Y%m%d%H%M%S`
 TAG="${TRAVIS_BRANCH}-${TIMESTAMP}-${GIT_SHORT_COMMIT}"
-IMAGE_NAME="centrifugeio/gateway"
+IMAGE_NAME="centrifugeio/tinlake-data-backend"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t "${IMAGE_NAME}:${TAG}" .
 docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:latest"
