03520837f84a966e58a14f9ee26880bc02c8c618
ilinzweilin <ilinzweilin@ilinzweilins-MacBook-Pro.local>
2019-08-28 11:53:39 +0200

    make db path configuravble through env

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,18 @@
 import EventAPI  from './services/graphqlsettings/datasources/event';
 import Datastore from 'nedb';
-require('dotenv').config()
-
-const datastore = new Datastore({ filename: './data/.tinlake_events.db', autoload: true });
-
-export const eventAPI = new EventAPI(datastore);
+import path from 'path';
+require('dotenv').config();
 
 export const config = {
   rpcUrl: process.env.RPC_URL,
   EthFromAddress: process.env.ETH_FROM_ADDRESS,
   cronInterval: 1,
   tinlakeAddresses: JSON.parse(process.env.TINLAKE_ADDRESSES),
-  environment: process.env.NODE_ENV || 'development'
+  environment: process.env.NODE_ENV || 'development',
+  dbPath: process.env.DB_PATH || './data/'
 };
+
+const dbFileName = path.join(config.dbPath, ".tinlake_events.db");
+const datastore = new Datastore({ filename: dbFileName, autoload: true });
+export const eventAPI = new EventAPI(datastore);
 export default config;
